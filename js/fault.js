59aacde2b08a15bda4228d540b300ba050a0093e
Oleksandr Yanovych <oleksandr.yanovych@xyleme.com>
2014-11-25 18:19:29 +0200

    animator class created

--- js/fault.js
+++ js/fault.js
@@ -93,6 +93,34 @@
         }
     }
 
+    var Animator = {
+        /**
+          * @desc runs render() on each frame reflow until render returns false
+          * @param function render - callback to call each frame reflow
+          * @param object element - optinally provided element to make accessible in animation loop
+          * @return void
+        */
+        this.animate = function animLoop( render, element ) {
+            var running, lastFrame = +new Date;
+
+            // main cycle
+            function loop( now ) {
+                // stop the loop if render returned false
+                if ( running !== false ) {
+                    requestAnimationFrame( loop, element );
+                    var deltaT = now - lastFrame;
+                    // do not render frame when deltaT is too high
+                    if ( deltaT < 160 ) {
+                        running = render( deltaT );
+                    }
+                    lastFrame = now;
+                }
+            }
+
+            loop( lastFrame );
+        }
+    }
+
     var ConnectionType = {
         STRAIGHT: 0,
         BEZIER: 1,
