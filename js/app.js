fbb3ddeb5be2dbf7c2cc57cb9100f844350e765a
Oleksandr Yanovych <oleksandr.yanovych@xyleme.com>
2014-12-01 15:30:26 +0200

    added sass support; close buttons restyled

--- js/app.js
+++ js/app.js
@@ -23,16 +23,47 @@ window.addEventListener('load', function() {
     };
 
     req.send(null);
-
-
-    //tree.drawLogicInput();
-    //tree.loopChildren();
-    //tree.getMaxInputNestedLevel();
-
 });
 
 function applicationStart(data) {
     window.tree = new FaultTree(Snap, 'surface');
     tree.init(data);
     window.json = tree.getJsonTree();
+    closeHandlersInit();
+}
+
+function $() {
+    var qs = document.querySelector.bind(document);
+    return qs.apply(this, [].slice.apply(arguments));
+}
+
+function hasClass(target, className) {
+    return new RegExp('(\\s|^)' + className + '(\\s|$)').test(target.className);
+}
+
+HTMLElement.prototype.hasClass = function(className) {
+    return hasClass(this, className);
+}
+
+HTMLElement.prototype.toggleClass = function(className) {
+    if (this.hasClass(className)) {
+        this.className = this.className.replace(className, '');
+    } else {
+        if (this.className) {
+            this.className += ' ';
+        }
+        this.className += className;
+    }
+}
+
+function closeHandlersInit() {
+    var leftbar = $('#leftbar'),
+        bottombar = $('#rightbottombar');
+    $('#hideLeft').addEventListener('click', function(e) {
+        leftbar.toggleClass('shrinkedHorizontal');
+    });
+
+    $('#hideBottom').addEventListener('click', function(e) {
+        bottombar.toggleClass('shrinkedVertical');
+    });
 }
